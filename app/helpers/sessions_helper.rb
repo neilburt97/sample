ebd6e3819ade5a1b21a681843440808a4e2d3a3e
neilburt97 <neilburt97@gmail.com>
2017-04-20 13:34:01 +0100

    add account activation

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,7 +9,7 @@ module SessionsHelper
 	def remember(user)
 		user.remember
 		cookies.permanent.signed[:user_id] = user.id
-		cookies.permanent.signed[:remember_token] = user.remember_token
+		cookies.permanent[:remember_token] = user.remember_token
 	end
 		
 	# Returns true if given user is current user.
@@ -17,13 +17,13 @@ module SessionsHelper
 		user == current_user
 	end
 	
-	# Return the user corresponding to teh remember token cookie.
+	# Return the current logged-in user (if any)
 	def current_user
 		if (user_id = session[:user_id])
-			@current_user ||= User.find_by(id: session[:user_id])
+			@current_user ||= User.find_by(id: user_id)
 		elsif (user_id = cookies.signed[:user_id])
 			user = User.find_by(id: user_id)
-			if user && user.authenticated?(cookies[:remember_token])
+			if user && user.authenticated?(:remember, cookies[:remember_token])
 				log_in user
 				@current_user = user
 			end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,9 @@ User.create!(name: 'Neil Mckenna',
 	email: "neilburt97@gmail.com",
 	password: "neil123",
 	password_confirmation: "neil123",
-	admin: true)
+	admin: true,
+	activated: true,
+	activated_at: Time.zone.now)
 
 99.times do |n|
 	name = Faker::Name.name
@@ -19,5 +21,7 @@ User.create!(name: 'Neil Mckenna',
 	User.create!(name: name,
 		email: email,
 		password: password,
-		password_confirmation: password)
+		password_confirmation: password,
+		activated: true,
+		activated_at: Time.zone.now)
 end
