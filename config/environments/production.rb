6d516aa333da5f8cb8a0c116dfb4c32a1657aa34
neilburt97 <neilburt97@gmail.com>
2017-04-20 18:00:18 +0100

    Add password reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors. 
 	
 	# Setup the mailer config
+	config.action_controller.default_url_options = {:host => request.env["SERVER_NAME"]}
 	config.action_mailer.perform_deliveries = true
 	config.action_mailer.raise_delivery_errors = true
 	config.action_mailer.delivery_method = :smtp
