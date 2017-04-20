4f399934c4439a65286ae70d2a1f3f0f4f6c712d
neilburt97 <neilburt97@gmail.com>
2017-04-20 18:13:59 +0100

    Add password reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,16 +61,15 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors. 
 	
 	# Setup the mailer config
-	config.action_controller.default_url_options = {
-		:host => "https://secret-lowlands.herokuapp.com"
-		}
+	host = 'secret-lowlands.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
 	config.action_mailer.perform_deliveries = true
 	config.action_mailer.raise_delivery_errors = true
 	config.action_mailer.delivery_method = :smtp
 	config.action_mailer.smtp_settings = {
 		:user_name => ENV["GMAIL_USERNAME"],
 		:password => ENV["GMAIL_PASSWORD"],
-		:domain => "secret-lowlands.herokuapp.com",
+		:domain => 'gmail.com',
 		:address => "smtp.gmail.com",
 		:port => 587,
 		:authentication => :plain,
