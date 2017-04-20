3cd173b85d430d93b973c7772709e4c4692ecc76
neilburt97 <neilburt97@gmail.com>
2017-04-20 15:12:28 +0100

    added gmail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,7 @@
 class UserMailer < ApplicationMailer
-
+	
+	default from: "neilburt97@gmail.com"
+	
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,12 +62,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'https://secret-lowlands.herokuapp.com' }  
 	config.action_mailer.delivery_method = :smtp
 	config.action_mailer.perform_deliveries = true
-	config.action_mailer.raise_delivery_errors = true
+	config.action_mailer.raise_delivery_errors = false
 	config.action_mailer.default :charset => "utf-8"
 	config.action_mailer.smtp_settings = {  
 		address: "smtp.gmail.com",
 		port: 587,
-		domain: "myapp.herokuapp.com",
+		domain: "secret-lowlands.herokuapp.com",
 		authentication: "plain",
 		enable_starttls_auto: true,
 		user_name: ENV["GMAIL_USERNAME"],
