b0ee8aa082c96416b6c1bb9ac7457d7d9d3e5ece
neilburt97 <neilburt97@gmail.com>
2017-05-21 00:31:10 +0100

    Fixed issue with email registration

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,9 +7,9 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.account_activation.subject
   #
-  def account_activation(user)
-    @user = user
-    mail to: user.email, subject: "Account activation"
+  def account_activation(@user)
+    #@user = user
+    mail to: @user.email, subject: "Account activation"
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -17,8 +17,8 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.password_reset.subject
   #
-  def password_reset(user)
-    @user = user
-    mail to: user.email, subject: "Password reset"
+  def password_reset(@user)
+    #@user = user
+    mail to: @user.email, subject: "Password reset"
   end
 end
